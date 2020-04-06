e1a62391e598d312fa98ac61d881782bd33a7af6
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-04-06 08:50:05 +0200

    Comuni update!

--- append.py
+++ append.py
@@ -1,6 +1,6 @@
 import pandas as pd
 df_full = pd.read_csv(r"dati-comuni/izsam-covid19-ita-comuni.csv")
-df_last = pd.read_csv(r"dati-comuni/izsam-covid19-ita-comuni-20200321.csv")
+df_last = pd.read_csv(r"dati-comuni/izsam-covid19-ita-comuni-20200403.csv")
 complete_df = df_full.append(df_last)
 complete_df.sort_values(by='data', ascending=False)
 complete_df.to_csv(r'dati-comuni/izsam-covid19-ita-comuni.csv',index=False)
